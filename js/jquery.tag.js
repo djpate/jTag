7f43e2975a58f1ff0d98755e5f3fa4515022a080
Christophe Verbinnen <djpate@gmail.com>
2011-01-13 00:22:07 +0100

    almost done !

--- js/jquery.tag.js
+++ js/jquery.tag.js
@@ -1,88 +1,110 @@
 (function($){
 	
-	$.fn.tag = function(options) {
+	$.extend($.fn,{
+		tag: function(options) {
 
-		var defaults = {
-			saveLabel: "Save",
-			minWidth: 100,
-			minHeight : 100,
-			defaultWidth : 100,
-			defaultHeight: 100,
-			maxHeight : null,
-			maxWidth : null,
-			save : null,
-			autoShowDrag: true
-		};
-		
-		var options = $.extend(defaults, options);  
+			var defaults = {
+				minWidth: 100,
+				minHeight : 100,
+				defaultWidth : 100,
+				defaultHeight: 100,
+				maxHeight : null,
+				maxWidth : null,
+				save : null,
+				autoShowDrag: true
+			};
+			
+			var options = $.extend(defaults, options);  
 
-		return this.each(function() {
-			
-			var obj = $(this);
-			
-			var addTag = function(width,height,top,left){
-				/* reset the drag box */
+			return this.each(function() {
+				
+				var obj = $(this);
+				
+				obj.data("options",options);
+				
+				obj.wrap('<div class="jTagArea" />');
+				
+				$(this).parent().width(obj.width());
+				$(this).parent().height(obj.height());
+				
 				if(options.autoShowDrag){
-					obj.siblings(".jTagDrag").animate({
-						top: 0,
-						left: 0,
-						width: options.defaultWidth,
-						height: options.defaultHeight
-					},1000);
-				} else {
-					obj.siblings(".jTagDrag").remove();
+					obj.showDrag();
 				}
 				
-				/* add the tag */
-				$('<div class="jTagTag" style="width:'+width+'px;height:'+height+'px;top:'+top+'px;left:'+left+'px;"><span>a Tag</span></div>').insertAfter(obj);
-			}
+			});
+		},
+		hideDrag: function(){
+			obj = $(this);
 			
-			var showDrag = function(){
-				
-				$(".jTagTag",obj).hide();
-				
-				$('<div class="jTagDrag"><div class="jTagSave">'+options.saveLabel+'</div></div>').insertAfter(obj);
-				
-				$(".jTagSave").click(function(){
-				
-					height = $(this).parent().height();
-					width = $(this).parent().width();
-					top = $(this).parent().attr('offsetTop');
-					left = $(this).parent().attr('offsetLeft');
-					
-					addTag(width,height,top,left);
-					
-					if(options.save){
-						options.save(width,height,top,left);
-					}
-					
-				});
-				
-				$(".jTagDrag").resizable({
-					containment: 'parent',
-					minWidth: options.minWidth,
-					minHeight: options.minHeight,
-					maxWidht: options.maxWidth,
-					maxHeight: options.maxHeight
-				
-				});
+			obj.siblings(".jTagDrag").remove();
+			$(".jTagTag",obj).show();
 			
-				$(".jTagDrag").draggable({
-					containment: 'parent'
-				});
-				
-			}
-			
-			obj.wrap('<div class="jTagArea" />');
-			
-			$(this).parent().width(obj.width());
-			$(this).parent().height(obj.height());
-			
-			if(options.autoShowDrag){
-				showDrag();
-			}
-			
-		});
+		},
+		showDrag: function(){
 		
-	};
+			obj = $(this);
+			
+			var options = obj.data('options');
+			
+			if(obj.siblings(".jTagDrag").length==1){
+				alert("You're allready tagging someone");
+				return;
+			}
+			
+			$(".jTagTag",obj.parent()).hide();
+					
+			$('<div class="jTagDrag"><div class="jTagSave"><input type="text"><div class="jTagSaveClose"></div><div class="jTagSaveBtn"></div></div>').insertAfter(obj);
+			
+			$(".jTagSaveBtn").click(function(){
+			
+				if($(this).prev().prev().val()==''){
+					alert('The label cannot be empty');
+					return;
+				}
+				
+				label = $(this).prev().prev().val();
+				height = $(this).parent().parent().height();
+				width = $(this).parent().parent().width();
+				top = $(this).parent().parent().attr('offsetTop');
+				left = $(this).parent().parent().attr('offsetLeft');
+				
+				obj.addTag(width,height,top,left,label);
+				
+				if(options.save){
+					options.save(width,height,top,left,label);
+				}
+				
+			});
+			
+			$(".jTagSaveClose").click(function(){
+				obj.hideDrag();
+			});
+			
+			$(".jTagDrag").resizable({
+				containment: 'parent',
+				minWidth: options.minWidth,
+				minHeight: options.minHeight,
+				maxWidht: options.maxWidth,
+				maxHeight: options.maxHeight
+			
+			});
+		
+			$(".jTagDrag").draggable({
+				containment: 'parent'
+			});
+		},
+		addTag: function(width,height,top,left,label){
+			
+			obj = $(this);
+			
+			var options = obj.data('options');
+			
+			$(".jTagTag",obj.parent()).show();
+			
+			$('<div class="jTagTag" style="width:'+width+'px;height:'+height+'px;top:'+top+'px;left:'+left+'px;"><span>'+label+'</span></div>').insertAfter(obj);
+			
+			obj.siblings(".jTagDrag").remove();
+			
+		}
+	});
 })(jQuery);
